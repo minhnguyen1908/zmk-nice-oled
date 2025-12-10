81d1040f1e7a6eb7e968878d6f98acaedda98921
Minh <minhnguyen1908@gmail.com>
2025-12-10 15:50:57 +0700

    Continue the update.

--- boards/shields/nice_oled/widgets/output.c
+++ boards/shields/nice_oled/widgets/output.c
@@ -1,3 +1,4 @@
+/*
 #include "output.h"
 #include "../assets/custom_fonts.h"
 #include <zephyr/kernel.h>
@@ -9,10 +10,10 @@ LV_IMG_DECLARE(usb);
 
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
 static void draw_usb_connected(lv_obj_t *canvas) {
-  /*lv_draw_image_dsc_t img_dsc;*/
-  /*lv_draw_image_dsc_init(&img_dsc);*/
+  // lv_draw_image_dsc_t img_dsc;
+  // lv_draw_image_dsc_init(&img_dsc);
 
-  /*lv_canvas_draw_image(canvas, 0, 34, &usb, &img_dsc);*/
+  // lv_canvas_draw_image(canvas, 0, 34, &usb, &img_dsc);
   // lv_canvas_draw_image(canvas, 45, 2, &usb, &img_dsc);
     // 1. Layer
     lv_layer_t layer;
@@ -36,9 +37,9 @@ static void draw_ble_unbonded(lv_obj_t *canvas) {
   lv_draw_image_dsc_init(&img_dsc);
 
   // 36 - 39
-  lv_canvas_draw_image(canvas, -1, 32, &bt_unbonded, &img_dsc);
+  // lv_canvas_draw_image(canvas, -1, 32, &bt_unbonded, &img_dsc);
   // lv_canvas_draw_image(canvas, 44, 0, &bt_unbonded, &img_dsc);
-  */
+
     lv_layer_t layer;
     lv_canvas_init_layer(canvas, &layer);
 
@@ -75,8 +76,96 @@ void draw_output_status(lv_obj_t *canvas, const struct status_state *state) {
   lv_draw_rect_dsc_t rect_white_dsc;
   init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
   lv_canvas_draw_rect(canvas, -3, 32, 24, 15, &rect_white_dsc);
-  */
 
+
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+  switch (state->selected_endpoint.transport) {
+  case ZMK_TRANSPORT_USB:
+    draw_usb_connected(canvas);
+    break;
+
+  case ZMK_TRANSPORT_BLE:
+    if (state->active_profile_bonded) {
+      if (state->active_profile_connected) {
+        draw_ble_connected(canvas);
+      } else {
+        draw_ble_disconnected(canvas);
+      }
+    } else {
+      draw_ble_unbonded(canvas);
+    }
+    break;
+  }
+#else
+  if (state->connected) {
+    draw_ble_connected(canvas);
+  } else {
+    draw_ble_disconnected(canvas);
+  }
+#endif
+}
+*/
+
+#include "output.h"
+#include "../assets/custom_fonts.h"
+#include <zephyr/kernel.h>
+
+LV_IMAGE_DECLARE(bt_no_signal);
+LV_IMAGE_DECLARE(bt_unbonded);
+LV_IMAGE_DECLARE(bt);
+LV_IMAGE_DECLARE(usb);
+
+#if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+static void draw_usb_connected(lv_obj_t *canvas) {
+    lv_layer_t layer;
+    lv_canvas_init_layer(canvas, &layer);
+
+    lv_draw_image_dsc_t img_dsc;
+    lv_draw_image_dsc_init(&img_dsc);
+    img_dsc.src = &usb;
+
+    lv_area_t coords = {0, 34, 0 + usb.header.w - 1, 34 + usb.header.h - 1};
+    lv_draw_image(&layer, &img_dsc, &coords);
+}
+
+static void draw_ble_unbonded(lv_obj_t *canvas) {
+    lv_layer_t layer;
+    lv_canvas_init_layer(canvas, &layer);
+
+    lv_draw_image_dsc_t img_dsc;
+    lv_draw_image_dsc_init(&img_dsc);
+    img_dsc.src = &bt_unbonded;
+
+    lv_area_t coords = {-1, 32, -1 + bt_unbonded.header.w - 1, 32 + bt_unbonded.header.h - 1};
+    lv_draw_image(&layer, &img_dsc, &coords);
+}
+#endif
+
+static void draw_ble_disconnected(lv_obj_t *canvas) {
+    lv_layer_t layer;
+    lv_canvas_init_layer(canvas, &layer);
+
+    lv_draw_image_dsc_t img_dsc;
+    lv_draw_image_dsc_init(&img_dsc);
+    img_dsc.src = &bt_no_signal;
+
+    lv_area_t coords = {4, 32, 4 + bt_no_signal.header.w - 1, 32 + bt_no_signal.header.h - 1};
+    lv_draw_image(&layer, &img_dsc, &coords);
+}
+
+static void draw_ble_connected(lv_obj_t *canvas) {
+    lv_layer_t layer;
+    lv_canvas_init_layer(canvas, &layer);
+
+    lv_draw_image_dsc_t img_dsc;
+    lv_draw_image_dsc_init(&img_dsc);
+    img_dsc.src = &bt;
+
+    lv_area_t coords = {4, 32, 4 + bt.header.w - 1, 32 + bt.header.h - 1};
+    lv_draw_image(&layer, &img_dsc, &coords);
+}
+
+void draw_output_status(lv_obj_t *canvas, const struct status_state *state) {
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
   switch (state->selected_endpoint.transport) {
   case ZMK_TRANSPORT_USB:
--- boards/shields/nice_oled/widgets/util.c
+++ boards/shields/nice_oled/widgets/util.c
@@ -1,3 +1,4 @@
+/*
 #include "util.h"
 #include <ctype.h>
 #include <zephyr/kernel.h>
@@ -21,7 +22,7 @@ void to_uppercase(char *str) {
   /*lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);*/
   /*[>lv_canvas_transform(canvas, &img, 900, LV_SCALE_NONE, -1, 0,<]*/
                       /*[>CANVAS_HEIGHT / 2, CANVAS_HEIGHT / 2, false);<]*/
-/*}*/
+/*}
 
 void rotate_canvas(lv_obj_t *canvas, lv_image_dsc_t *img) {
     (void)canvas;
@@ -33,7 +34,7 @@ void draw_background(lv_obj_t *canvas) {
   init_rect_dsc(&rect_black_dsc, LVGL_BACKGROUND);
 
   /*lv_canvas_draw_rect(canvas, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT,*/
-                      /*&rect_black_dsc);*/
+                      /*&rect_black_dsc);
     // 1. Declare and Init the descriptor
     lv_draw_rect_dsc_t draw_dsc;
     init_rect_dsc(&draw_dsc, LVGL_BACKGROUND);
@@ -67,9 +68,62 @@ void init_rect_dsc(lv_draw_rect_dsc_t *rect_dsc, lv_color_t bg_color) {
   /*lv_draw_line_dsc_init(line_dsc);*/
   /*line_dsc->color = color;*/
   /*line_dsc->width = width;*/
-/*}*/
+/*}
 
 // FIX: New helper for image descriptors
+void init_image_dsc(lv_draw_image_dsc_t *image_dsc, const void *src) {
+    lv_draw_image_dsc_init(image_dsc);
+    image_dsc->src = src;
+}*/
+
+#include "util.h"
+#include <ctype.h>
+
+void to_uppercase(char *str) {
+    for (int i = 0; str[i]; i++) {
+        str[i] = toupper(str[i]);
+    }
+}
+
+void rotate_canvas(lv_obj_t *canvas, lv_image_dsc_t *img) {
+    // LVGL 9 removed direct canvas transformation.
+    (void)canvas;
+    (void)img;
+}
+
+void draw_background(lv_obj_t *canvas) {
+    lv_draw_rect_dsc_t draw_dsc;
+    init_rect_dsc(&draw_dsc, LVGL_BACKGROUND);
+
+    lv_layer_t layer;
+    lv_canvas_init_layer(canvas, &layer);
+
+    lv_area_t coords = {0, 0, CANVAS_WIDTH - 1, CANVAS_HEIGHT - 1};
+    lv_draw_rect(&layer, &draw_dsc, &coords);
+}
+
+void init_rect_dsc(lv_draw_rect_dsc_t *rect_dsc, lv_color_t bg_color) {
+    lv_draw_rect_dsc_init(rect_dsc);
+    rect_dsc->bg_color = bg_color;
+}
+
+// THIS WAS MISSING!
+void init_line_dsc(lv_draw_line_dsc_t *line_dsc, lv_color_t color, uint8_t width) {
+    lv_draw_line_dsc_init(line_dsc);
+    line_dsc->color = color;
+    line_dsc->width = width;
+    line_dsc->round_start = 1;
+    line_dsc->round_end = 1;
+}
+
+void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color,
+                    const lv_font_t *font, lv_text_align_t align) {
+    lv_draw_label_dsc_init(label_dsc);
+    label_dsc->color = color;
+    label_dsc->font = font;
+    label_dsc->align = align;
+}
+
 void init_image_dsc(lv_draw_image_dsc_t *image_dsc, const void *src) {
     lv_draw_image_dsc_init(image_dsc);
     image_dsc->src = src;
